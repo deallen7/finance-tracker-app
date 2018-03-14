5fd19442da49278dd4d5d2d190d8083ed40a9988
David Allen <davideallen7@gmail.com>
2018-03-14 13:31:51 -0700

    add friends and delete friends

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  get 'add_friend', to: "users#add_friend"
 
 end
